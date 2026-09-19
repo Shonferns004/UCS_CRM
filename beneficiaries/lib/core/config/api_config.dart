48e033806cf8d03478ce2128a65e973601258562
Shonferns004 <shawnferns004@gmail.com>
2026-09-19 11:51:30 +0530

    chore: rename apps/client to apps/attendance, migrate ucs crm to client, remove legacy duplicates, ignore build outputs and secrets

--- beneficiaries/lib/core/config/api_config.dart
+++
@@ -1,6 +0,0 @@
-class Config {
-  static String apiBaseUrl = 'https://api.beingsevak.org/api';
-  static String socketUrl = 'https://api.beingsevak.org';
-  static const String appName = 'Beneficiaries';
-  static const String appBrand = 'BEING SEVAK';
-}
